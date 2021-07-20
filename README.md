046ea89631126a4f8a63832043a37a65673c68df
statmlben <bendaimath@gmail.com>
2021-07-20 20:14:37 +0800

    update logo and dataset

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ![logo](./figs/logo.png)
 
-This is a repository on code and notebook illustration for CUHK-STAT3009. [ [Homepage](https://www.bendai.org/STAT3009/) + [GitHub](https://github.com/statmlben/CUHK-STAT3009) + [Slices]() ]
+This is a repository on code and notebook illustration for CUHK-STAT3009. [ [Homepage](https://www.bendai.org/STAT3009/) + [GitHub](https://github.com/statmlben/CUHK-STAT3009) + [Slides]() ]
 
 - [Notebook1](notebook1.ipynb): Baseline methods for Recommender Systems
 - [Notebook2](notebook2.ipynb): Correlation-based Recommender Systems
---
+++ dataset/pre-process.py
@@ -0,0 +1,39 @@
+import os
+import pandas as pd
+
+if not os.path.isfile('data.csv'):
+    data = open('data.csv', mode='w')
+
+file = 'combined_data_1.txt'
+
+# Remove the line with movie_id: and add a new column of movie_id
+print("Opening file: {}".format(file))
+with open(file) as f:
+	for line in f:
+		line = line.strip()
+		if line.endswith(':'):
+			movie_id = line.replace(':', '')
+		else:
+			data.write(movie_id + ',' + line)
+			data.write('\n')
+data.close()
+
+# Read all data into a pd dataframe
+df = pd.read_csv('data.csv', names=['movie_id', 'user_id','rating','date'])
+
+
+from sklearn import preprocessing
+le_movie = preprocessing.LabelEncoder()
+df['movie_id'] = le_movie.fit_transform(df['movie_id'])
+le_user = preprocessing.LabelEncoder()
+df['user_id'] = le_user.fit_transform(df['user_id'])
+
+# save the pre-processed data
+df.to_csv('data.csv')
+
+# train-test splitting
+from sklearn.model_selection import train_test_split
+dtrain, dtest = train_test_split(df, test_size=0.5, random_state=42)
+
+dtrain.to_csv('train.csv', index=False)
+dtest.to_csv('test.csv', index=False)
